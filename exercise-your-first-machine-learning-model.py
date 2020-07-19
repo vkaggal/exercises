e8481f800f5359c1b0bc7b02e89cbfc352f3b211
Kaggal, Vinod C <kaggal.vinod@mayo.edu>
2020-07-19 13:18:31 -0500

    Added a new kernel

--- exercise-your-first-machine-learning-model.py
+++ exercise-your-first-machine-learning-model.py
@@ -1,93 +1,96 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# **[Machine Learning Course Home Page](https://www.kaggle.com/learn/machine-learning)**
+# **[Introduction to Machine Learning Home Page](https://www.kaggle.com/learn/intro-to-machine-learning)**
 # 
 # ---
 # 
 
-# This exercise will test your ability to read a data file and understand statistics about the data.
-# 
-# In later exercises, you will apply techniques to filter the data, build a machine learning model, and iteratively improve your model.
-# 
-# The course examples use data from Melbourne. To ensure you can apply these techniques on your own, you will have to apply them to a new dataset (with house prices from Iowa).
-# 
-# The exercises use a "notebook" coding environment.  In case you are unfamiliar with notebooks, we have a [90-second intro video](https://www.youtube.com/watch?v=4C2qMnaIKL4).
-# 
-# # Exercises
-# 
-# Run the following cell to set up code-checking, which will verify your work as you go.
+# ## Recap
+# So far, you have loaded your data and reviewed it with the following code. Run this cell to set up your coding environment where the previous step left off.
 
 # In[ ]:
 
 
-# Set up code checking
-from learntools.core import binder
-binder.bind(globals())
-from learntools.machine_learning.ex2 import *
-print("Setup Complete")
-
-
-# ## Step 1: Loading Data
-# Read the Iowa data file into a Pandas DataFrame called `home_data`.
-
-# In[ ]:
-
-
+# Code you have previously used to load data
 import pandas as pd
 
 # Path of the file to read
 iowa_file_path = '../input/home-data-for-ml-course/train.csv'
 
-# Fill in the line below to read the file into a variable home_data
 home_data = pd.read_csv(iowa_file_path)
 
-# Call line below with no argument to check that you've loaded the data correctly
+# Set up code checking
+from learntools.core import binder
+binder.bind(globals())
+from learntools.machine_learning.ex3 import *
+
+print("Setup Complete")
+
+
+# In[ ]:
+
+
+#home_data.describe()
+home_data.columns
+
+
+# # Exercises
+# 
+# ## Step 1: Specify Prediction Target
+# Select the target variable, which corresponds to the sales price. Save this to a new variable called `y`. You'll need to print a list of the columns to find the name of the column you need.
+# 
+
+# In[ ]:
+
+
+# print the list of columns in the dataset to find the name of the prediction target
+home_data.columns
+
+
+# In[ ]:
+
+
+y = home_data.SalePrice
+
+# Check your answer
 step_1.check()
 
 
 # In[ ]:
 
 
-# Lines below will give you a hint or solution code
-#step_1.hint()
-#step_1.solution()
+# The lines below will show you a hint or the solution.
+# step_1.hint() 
+# step_1.solution()
 
 
-# ## Step 2: Review The Data
-# Use the command you learned to view summary statistics of the data. Then fill in variables to answer the following questions
-
-# In[ ]:
-
-
-# Print summary statistics in next line
-home_data.describe()
-
-
-# In[ ]:
-
-
-avg_lot_size = home_data['LotArea'].mean()
-print("Lot Area/len:(avg)",  avg_lot_size)
-
-from datetime import date
-newest_home_year = pd.to_datetime(home_data['YearBuilt'], format='%Y').max()
-print(date.today().year, newest_home_year.year)
-
-print(date.today().year - pd.to_datetime(home_data['YearBuilt'], format='%Y').max().year)
-
+# ## Step 2: Create X
+# Now you will create a DataFrame called `X` holding the predictive features.
+# 
+# Since you want only some columns from the original data, you'll first create a list with the names of the columns you want in `X`.
+# 
+# You'll use just the following columns in the list (you can copy and paste the whole list to save some typing, though you'll still need to add quotes):
+#     * LotArea
+#     * YearBuilt
+#     * 1stFlrSF
+#     * 2ndFlrSF
+#     * FullBath
+#     * BedroomAbvGr
+#     * TotRmsAbvGrd
+# 
+# After you've created that list of features, use it to create the DataFrame that you'll use to fit the model.
 
 # In[ ]:
 
 
-# What is the average lot size (rounded to nearest integer)?
-avg_lot_size = int(round(home_data['LotArea'].mean()))
+# Create the list of features below
+feature_names = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath', 'BedroomAbvGr', 'TotRmsAbvGrd']
 
-from datetime import date
-# As of today, how old is the newest home (current year - the date in which it was built)
-newest_home_age = date.today().year - pd.to_datetime(home_data['YearBuilt'], format='%Y').max().year
+# Select data corresponding to features in feature_names
+X = home_data[feature_names]
 
-# Checks your answers
+# Check your answer
 step_2.check()
 
 
@@ -98,24 +101,93 @@ step_2.check()
 # step_2.solution()
 
 
-# ## Think About Your Data
+# ## Review Data
+# Before building a model, take a quick look at **X** to verify it looks sensible
+
+# In[ ]:
+
+
+# Review data
+# print description or statistics from X
+#print(_)
+#X.describe
+
+# print the top few lines
+#print(_)
+X.head()
+
+
+# ## Step 3: Specify and Fit Model
+# Create a `DecisionTreeRegressor` and save it iowa_model. Ensure you've done the relevant import from sklearn to run this command.
 # 
-# The newest house in your data isn't that new.  A few potential explanations for this:
-# 1. They haven't built new houses where this data was collected.
-# 1. The data was collected a long time ago. Houses built after the data publication wouldn't show up.
+# Then fit the model you just created using the data in `X` and `y` that you saved above.
+
+# In[ ]:
+
+
+from sklearn.tree import DecisionTreeRegressor
+#specify the model. 
+#For model reproducibility, set a numeric value for random_state when specifying the model
+iowa_model = DecisionTreeRegressor(random_state=42)
+
+# Fit the model
+iowa_model.fit(X, y)
+
+# Check your answer
+step_3.check()
+
+
+# In[ ]:
+
+
+# step_3.hint()
+# step_3.solution()
+
+
+# ## Step 4: Make Predictions
+# Make predictions with the model's `predict` command using `X` as the data. Save the results to a variable called `predictions`.
+
+# In[ ]:
+
+
+predictions = iowa_model.predict(X)
+print(predictions)
+
+# Check your answer
+step_4.check()
+
+
+# In[ ]:
+
+
+# step_4.hint()
+# step_4.solution()
+
+
+# ## Think About Your Results
 # 
-# If the reason is explanation #1 above, does that affect your trust in the model you build with this data? What about if it is reason #2?
+# Use the `head` method to compare the top few predictions to the actual home values (in `y`) for those same homes. Anything surprising?
 # 
-# How could you dig into the data to see which explanation is more plausible?
-# 
-# Check out this **[discussion thread](https://www.kaggle.com/learn-forum/60581)** to see what others think or to add your ideas.
+
+# In[ ]:
+
+
+# You can write code in this cell
+print(iowa_model.predict(X.head(4)),"\n",y.head(4))
+
+
+# It's natural to ask how accurate the model's predictions will be and how you can improve that. That will be you're next step.
 # 
 # # Keep Going
 # 
-# You are ready for **[Your First Machine Learning Model](https://www.kaggle.com/dansbecker/your-first-machine-learning-model).**
+# You are ready for **[Model Validation](https://www.kaggle.com/dansbecker/model-validation).**
 # 
 
 # ---
-# **[Machine Learning Course Home Page](https://www.kaggle.com/learn/machine-learning)**
+# **[Introduction to Machine Learning Home Page](https://www.kaggle.com/learn/intro-to-machine-learning)**
 # 
 # 
+# 
+# 
+# 
+# *Have questions or comments? Visit the [Learn Discussion forum](https://www.kaggle.com/learn-forum/161285) to chat with other Learners.*
